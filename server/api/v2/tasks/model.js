cd8264b97ce0bd756bde90d59757c99a96a6409b
Luis Caicedo <sebastiancaicedo@uninorte.edu.co>
2022-05-03 13:27:27 -0500

    Mongoose Schema for tasks
    - Defined schema
    - timestamps added (createdAt & updatedAt)

--- server/api/v2/tasks/model.js
+++ server/api/v2/tasks/model.js
@@ -3,10 +3,19 @@ const mongoose = require('mongoose');
 const fields = {
   description: {
     type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 255,
   },
   author: {
     type: String,
+    required: false,
+    maxlength: 32,
   },
 };
 
-module.exports = { fields, Model: mongoose.model('task', fields) };
+const task = new mongoose.Schema(fields, {
+  timestamps: true,
+});
+
+module.exports = { fields, Model: mongoose.model('task', task) };
